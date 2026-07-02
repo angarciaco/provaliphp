a9822e633a8a0aa6c1c3b4e39c6135be5f730ba0
Miguel Angel Garcia (WSL 2026) <engmiguelgarcia@gmail.com>
2026-07-02 16:04:08 -0500

    Adding code for this third branch with a couple of errors.

---
+++ thbranch.php
@@ -0,0 +1,7 @@
+<?php
+
+// Crear un objeto DateTime con la fecha y hora actual
+$fecha = new DDateTime("now", new DateTimeZone("GMT"));
+
+// Imprimir en el formato deseado (ejemplo: YYYY-MM-DD HH:MM:SS)
+echo $fecha->format("Y-m-d H:i:s");
